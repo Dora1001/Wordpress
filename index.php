815f2035fc5ab66d6ffdfc9b447c2ca582be3195
Dorota Janus <dorotaja101@gmail.com>
2021-03-19 12:16:48 +0100

    index - done

--- index.php
+++ index.php
@@ -1,2 +1,54 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+
+<link rel="shortcut icon" href="/favicon.ico">
+<!-- Stylesheets -->
+<link href="<?php echo get_bloginfo('template_directory')?>/style.css" >
+
+<?php wp_head()?>
+
+</head>
+
+<div class="container">
+
+<div class="menu">
+<div class="menu__logo">
+
+</div>
+</div>
+
+
+<div class="slider">
+<h1 class="slider__header">Heading</h1>
+<p class="slider__text">Lorem ipsum dolor sit amet, consect etur adipiscing elit.</p>
+</div>
+
+
+<section class="section">
+<h1 class="section__header">Heading<h1>
+<p class="section__text">Lorem ipsum dolor sit amet, consect etur adipiscing elit.</p>
+
+<div class="section__components">
+	<h2 class="section__title">Heading<h2>
+		<div class="section__component"></div>
+		<div class="section__component"></div>
+		<div class="section__component"></div>
+</div>
+
+</section>
+
+
+</div>
+<body>
+
+
+
+<?php wp_footer() ?>
+</body>
+</html>
+
 
